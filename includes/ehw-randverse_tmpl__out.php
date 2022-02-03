04fae45811ecd6d452dbf3488e9ea2c6e59c9a7b
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-03 16:10:08 -0600

    UPDATE ehw-randverse_tmpl__out.php: Add book, chapter, and verse

--- includes/ehw-randverse_tmpl__out.php
+++ includes/ehw-randverse_tmpl__out.php
@@ -1,8 +1,8 @@
 <?php /* ehw-randverse-scripts.php */
 
-$verse = 'For I know the thoughts that I think toward you,' . 
+$verse1 = 'For I know the thoughts that I think toward you,' . 
          'saith the LORD, thoughts of peace, and not of evil,' . 
-         'to give you an expected end.';
+         'to give you an expected end. -- Jeremiah 29:11';
 
-$output = $verse;
+$output = $verse1;
 
