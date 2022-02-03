6075374db37ba192c906d6cd6c6f5a49797fcf82
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-03 14:00:22 -0600

    UPDATE ehw-randverse-scripts.php: Replace hard-code textdomain with vars

--- includes/ehw-randverse-scripts.php
+++ includes/ehw-randverse-scripts.php
@@ -1,16 +1,16 @@
 <?php /* youtubesubs-scripts.php */
 
   // Add Scripts
-  function yts_add_scripts() {
+  function rbv_add_scripts() {
       // Add Main CSS
-      wp_enqueue_style('yts-main-style', plugins_url() . '/youtubesubs/css/style.css');
+      wp_enqueue_style('rbv-main-style', plugins_url() . "/$textdomain/css/style.css");
       
       // Add Main JS
-      wp_enqueue_script('yts-main-script', plugins_url() . '/youtubesubs/js/main.js');
+      wp_enqueue_script('rbv-main-script', plugins_url() . "/$textdomain/js/main.js");
       
       // Add Google Scripts
       wp_register_script('google', 'https://apis.google.com/js/platform.js');
       wp_enqueue_script('google');
   }
   
-  add_action('wp_enqueue_scripts', 'yts_add_scripts');
+  add_action('wp_enqueue_scripts', 'rbv_add_scripts');
