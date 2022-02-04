51defbb21495944182981e3149375d97950e98d8
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-04 06:55:11 -0600

    UPDATE ehw-randverse_tmpl__out.php: Change $verses to assoc array (3 dim)

--- includes/ehw-randverse_tmpl__out.php
+++ includes/ehw-randverse_tmpl__out.php
@@ -1,5 +1,6 @@
 <?php /* ehw-randverse-scripts.php */
 
+
 $verse1 = 'For I know the thoughts that I think toward you,' . 
 'saith the LORD, thoughts of peace, and not of evil,' . 
 'to give you an expected end. -- Jeremiah 29:11';
@@ -12,9 +13,15 @@ $verse3 = 'And we know that all things work together for good to '.
 'them that love God, to them who are the called according to his'.
 'purpose. -- Romans 8:28';
 
-$verses = [$verse1, $verse2, $verse3];
+$verses['Jeremiah'][29][11] = $verse1;
+$verses['John'][3][16] = $verse2;
+$verses['Romans'][8][28] = $verse3;
 
-$output = $verses[array_rand($verses)];
+
+// $verses = [$verse1, $verse2, $verse3];
+$rand_keys = array_rand($verses);
+
+// $output = $verses[];
 
 ?>
 
