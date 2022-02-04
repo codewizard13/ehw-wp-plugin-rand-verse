cb9cf1222b2cdb5fadec33e18ae99f8439363cb3
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-04 12:04:57 -0600

    UPDATE ehw-randverse_tmpl__out.php: Remove cite from verses 1-3

--- includes/ehw-randverse_tmpl__out.php
+++ includes/ehw-randverse_tmpl__out.php
@@ -13,15 +13,15 @@
 
 $verse1 = 'For I know the thoughts that I think toward you,' . 
 'saith the LORD, thoughts of peace, and not of evil,' . 
-'to give you an expected end. -- Jeremiah 29:11';
+'to give you an expected end.';
 
 $verse2 = 'For God so loved the world, that he gave his only '. 
 'begotten Son, that whosoever believeth in him should not perish,'.
-'but have everlasting life. -- John 3:16';
+'but have everlasting life.';
 
 $verse3 = 'And we know that all things work together for good to '.
 'them that love God, to them who are the called according to his'.
-'purpose. -- Romans 8:28';
+'purpose.';
 
 $verses['Jeremiah'][29][11] = $verse1;
 $verses['John'][3][16] = $verse2;
