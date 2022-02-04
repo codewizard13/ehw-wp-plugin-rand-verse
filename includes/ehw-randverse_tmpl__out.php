9079879f5c6a8fab6fd491fbe8c3b1c20f6d80bd
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-04 05:45:38 -0600

    UPDATE ehw-randverse_tmpl__out.php: Add 2 more verse string vars. Declare empty $verses arr

--- includes/ehw-randverse_tmpl__out.php
+++ includes/ehw-randverse_tmpl__out.php
@@ -1,10 +1,19 @@
 <?php /* ehw-randverse-scripts.php */
-?>
 
-<?php
 $verse1 = 'For I know the thoughts that I think toward you,' . 
-         'saith the LORD, thoughts of peace, and not of evil,' . 
-         'to give you an expected end. -- Jeremiah 29:11';
+'saith the LORD, thoughts of peace, and not of evil,' . 
+'to give you an expected end. -- Jeremiah 29:11';
+
+$verse2 = 'For God so loved the world, that he gave his only '. 
+'begotten Son, that whosoever believeth in him should not perish,'.
+'but have everlasting life. -- John 3:16';
+
+$verse3 = 'And we know that all things work together for good to '.
+'them that love God, to them who are the called according to his'.
+'purpose. -- Romans 8:28';
+
+$verses = [];
+
 
 $output = $verse1;
 
