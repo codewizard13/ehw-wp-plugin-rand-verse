7c6dd046633bbb6d7864598715d18611682b9740
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-04 11:47:08 -0600

    UPDATE demo-tree-recurse/tree.php: ADD header boilerplate comment

--- includes/demo-tree-recurse/tree.php
+++ includes/demo-tree-recurse/tree.php
@@ -1,4 +1,24 @@
 <?php
+/*
+Project Name:   EHW WP Plugin: EHW Random Bible Verse
+Main Prj File:  ehw-randverse.php
+
+This Filename:  tree.php
+Date Created:   02/04/22
+Date Updated:   --
+Programmer:     Eric L. Hepperle
+
+File Version:    1.00.00
+
+File Purpose:
+Defined treeOut() function for this demo.
+
+Requires:
+* index.php
+
+Future:
+* demo-tree-recurse/ dir and contents should be moved to own project
+*/
 
 function treeOut($tree) {
    $markup = '';
