bd279edf25c02242f597112607e96111cd8b80eb
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-04 11:45:17 -0600

    UPDATE demo-tree-recurse/index.php: ADD header boilerplate comment

--- includes/demo-tree-recurse/index.php
+++ includes/demo-tree-recurse/index.php
@@ -1,4 +1,28 @@
 <?php echo "I'm in " . basename(__FILE__);
+/*
+Project Name:   EHW WP Plugin: EHW Random Bible Verse
+Main Prj File:  ehw-randverse.php
+
+This Filename:  index.php
+Date Created:   02/04/22
+Date Updated:   --
+Programmer:     Eric L. Hepperle
+
+File Version:    1.00.00
+
+File Purpose:
+Demonstrates PHP recursive tree walking. Demo outputs an indented
+unordered list for a multidimensional array three levels deep.
+
+- Create $tree variable (nested three dimensional array)
+- Call treeOut() function
+
+Requires:
+* tree.php
+
+Future:
+* demo-tree-recurse/ dir and contents should be moved to own project
+*/
 
 require('tree.php');
 
