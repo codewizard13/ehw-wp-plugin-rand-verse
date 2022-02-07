7fdd0a741e31f3313d06f870d1c3c08285b80ef3
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-07 07:04:58 -0600

    UPDATE ehw-randverse_tmpl__out.php: Change this filename comment

--- includes/ehw-randverse_tmpl__out.php
+++ includes/ehw-randverse_tmpl__out.php
@@ -1,4 +1,4 @@
-<?php /* ehw-randverse-scripts.php */
+<?php /* ehw-randverse_tmpl__out.php */
 
 // SAMPLE $verses array (King James Version, KJV):
 $verses['Jeremiah'][29][11] = 'For I know the thoughts that I think toward you, saith the LORD, thoughts of peace, and not of evil, to give you an expected end.';
