4f2d76ad785da4a0165e29487295f4d6989ac9ff
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-07 06:55:35 -0600

    UPDATE ehw-randverse_tmpl__out.php: Add detail to displayFormattedVerses() func comment

--- includes/ehw-randverse_tmpl__out.php
+++ includes/ehw-randverse_tmpl__out.php
@@ -92,8 +92,15 @@ echo "<H2>VERSES FORMATTED:</H2>";
 var_dump($my_verse_ids);
 
 /**
+ * @purpose:
+ * Loops through a verses array and prints all results to screen
+ * in HTML format. Styling is outside scope of this function.
+ * 
  * @args:
  * - Array $va: array of verses built with buildFormattedVerses()
+ * 
+ * @returns:
+ * - VOID
  */
 function displayFormattedVerses($va) {
    foreach ($va as $verse => $key) {
