a37cc735581a48356c303b72ad1bc140d412e836
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-10 12:00:09 -0600

    UPDATE ehw-randverse-scripts.php: Convert plugins_url() to use __FILE__

--- includes/ehw-randverse-scripts.php
+++ includes/ehw-randverse-scripts.php
@@ -3,10 +3,10 @@
   // Add Scripts
   function rbv_add_scripts() {
       // Add Main CSS
-      wp_enqueue_style('rbv-main-style', plugins_url() . "/ehw-wp-plugin-rand-verse/css/style.css");
+      wp_enqueue_style('rbv-main-style', plugins_url('../css/style.css', __FILE__));
       
       // Add Main JS
-      wp_enqueue_script('rbv-main-script', plugins_url() . "/ehw-wp-plugin-rand-verse/js/main.js");
+      wp_enqueue_script('rbv-main-script', plugins_url('../js/main.js', __FILE__));
       
       // Add Google Scripts
       wp_register_script('google', 'https://apis.google.com/js/platform.js');
